a99351daafad9d10164de932d4a8c2d10b6dbacb
Professor <Professor@SN-367469.edu.fiesc.com.br>
2026-02-03 14:22:30 -0300

    all is working

--- Atividades_03-02/Conversor_escala/Exercicio5.java
+++ Atividades_03-02/Conversor_escala/Exercicio5.java
@@ -11,8 +11,13 @@ Objetivo: Criar um método reutilizável para converter dados brutos de um PLC.
 public class Exercicio5 { 
     public static void main(String[] args) { 
     double valorBruto = 512; 
-    double resultado = converterEscala(valorBruto); 
+    String resultado = String.format("%.2f", converterEscala(valorBruto));
     System.out.println("O valor na escala de 0-100% é: " + resultado); 
     } 
-    // Cria aqui o teu método estático converterEscala 
-    } 
+
+
+    public static double converterEscala(double valorBruto){
+        return (valorBruto / 1023.0) * 100;
+
+    }
+} 
